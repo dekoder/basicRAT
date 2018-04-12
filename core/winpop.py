4d82c29f20019e9e2c989b1edf40f6dcb2f20b3d
mengyuan.ymy <mengyuan.ymy@alibaba-inc.com>
2018-04-12 13:53:11 +0800

    add windows pop

---
+++ core/winpop.py
@@ -0,0 +1,29 @@
+﻿#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+import win32api, win32con
+import win32ui
+import threading
+import time
+import os,sys
+
+def btnClose():
+    time.sleep(20)
+    try:
+        hd = win32ui.FindWindow(None, "scan")
+        hd.SendMessage(win32con.WM_CLOSE)
+    except:
+        pass
+    win32api.MessageBox(0, "The scan is complete, no virus was not found",  "scan", win32con.MB_OK) #u"扫描完成，未发现病毒！"
+
+def pop():
+    thread = threading.Thread(target=btnClose)
+    thread.start()
+
+    win32api.MessageBox(0, u"Background scanning...", "scan", win32con.MB_OK)
+
+    thread.join()
+
+
+
